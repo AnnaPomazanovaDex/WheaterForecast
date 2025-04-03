c52ffa65b5fca13e4ccfed5353d2b3a259ec3ea8
Anna Pomazanova <anna.pomazanova@dexcom.com>
2025-04-03 11:36:25 +0300

    Added project

---
+++ WheatherApp/WheatherApp/WheatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherAppApp.swift
+//  WheatherApp
+//
+//  Created by Anna Pomazanova on 03.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WheatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
